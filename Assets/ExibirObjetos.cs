de58799ad032d080f5377c7c6ea224fc8797c3ee
Lazaro Fernandes <lazaro.fernandes@lambda3.com.br>
2017-08-18 17:06:41 -0300

    Initial Commit

---
+++ Assets/ExibirObjetos.cs
@@ -0,0 +1,9 @@
+﻿using HUX.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExibirObjetos : InteractibleObject {
+
+    
+}
---
+++ Assets/ToggleReceiver.cs
@@ -0,0 +1,16 @@
+﻿using HUX.Interaction;
+using HUX.Receivers;
+using UnityEngine;
+
+public class ToggleReceiver : InteractionReceiver
+{
+    private bool _toggled;
+
+    protected override void OnTapped(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+    {
+        _toggled = !_toggled;
+
+        for (int i = 0; i < Targets.Count; i++)
+            Targets[i].SetActive(_toggled);
+    }
+}
